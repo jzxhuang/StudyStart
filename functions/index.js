1886835afe2741c5de67675f27a8bc2b4825a69e
Jeffrey Huang <jeff.huang62@gmail.com>
2018-03-04 09:40:21 -0500

    Add static site to add notes. Push facts to database working.

--- functions/index.js
+++ functions/index.js
@@ -36,7 +36,21 @@ const TOPIC_CANCEL = 'topic.cancel';
 const imageUrl = 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png';
 const linkUrl = 'https://assistant.google.com/';
 
-// creds go here
+admin.initializeApp({
+    databaseURL: "https://study-start-d6061.firebaseio.com",
+    credential: admin.credential.cert({
+        "type": "service_account",
+        "project_id": "study-start-d6061",
+        "private_key_id": "db418a82b1a2f00452daf14de3bc3182d0856317",
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDPckC5oUE+EFHd\ngaW+zvV9ayreofCI5er/BeCcoaHlK4w8Xb4EbPncD6Ia0Y67uXIRD3PY1wELkVt+\nmgN8LOjeDTaGy8VvOL+eHbjRhkYUm1su154M9JWzPcyHA8TghqlDyB/McFcjZv0y\n+txdxXkdNU318IMWFCzt+7c9FsUvXyF2ip05EKyrDJoCNwJ787GS8ceOom7WklYw\nasY7evJZDRYlFykz0OMejORn45XXQNf4pbmbWEn56Wmfvc0bxZsBkT++CeVBO1aF\n5EjzQJ4w4pH7rgxl/NJcD/q0/uhVqqqn4b4Krdo/A8OgXs1gQfzD9bYptqPqggGW\nuwdA0+NFAgMBAAECggEARRASCVrsqBdx0c9YWWJ4a0VS8+Sw+jqlVjrkMPFEv1GU\nsgI5gSqwsnlH7CrJHKlcFt7woAfQcsP1VHw83cnrywVWGmFbvTAE50SY47SSv0iL\nflhNZ53D+vT/kxGHqYJbdtP49iyObhxa/m5zEAruy/ETn+XvFlooLSDIu99JTYAB\n8BgWoI1wyXS1g2tx8ZlM6pK3MaPFEcfkV1fqX23Ff1Kvnp5TnqLaJsSG8qGBaItC\nPb5fwFTLbq/R91Ci9SuS1OPIP5awjhzrEzmOytb4NAEuVhB4JypFw7KOkjhUFu72\nhAkvO7HfsfvlsSe9Nxtjw0hpPIt/A9gCPD+WYN+PTwKBgQD5LFXvXA1MUajlxDr8\nSods4HDomUY95dFnnCT3R6ntVZ90UrztKZJ6eCedLAErL7A5XmVSWXR4BFwnE53t\ncUSLlUVsjiowflcEu/A7C+Ip5pYGICoK4YjZH92EAkAUnTEY4D1NgiVzZFTlDsy1\n7i7KeN69YnSZilln6IGjFg90YwKBgQDVIUA41EnuBD9xbpbu0HCJ/qxtWpIgRdxP\nUnzaHMr41VCU29zi6NP8UnjaPiND7b0vQruJfgoYtFqAIJU/wczKu05UD0kk/EXx\nbDiubqe1snGkgAdSKGhAwsdRs0fL3LfPAOXXyt1wbJ57Yzm5jAKJcfotqoVOWQNI\nNVVomMc2NwKBgQD13YySGfe5Nd59kcpORdApW7GtoghQbWPaY5WKNrlyyYkEg9uN\n/PZfSA52pQQmM/EkOoOp5UDqaAryOAYeVCS5wrn1KrZwkUmWD0g1Sz2H8NJaCwTR\nJcgdzraKWpoWfSEEFcgHogMIQJkQSDEw12xuNyQ6JVs+YehkgmTgopTvVwKBgB1b\nNiBdxU0ekqqKJAoC9e1cAd4SIpyFYuIeJ4pXevGpWC8Y1aT/kutnZLQS6QKbRXq0\nA2bd0IA0n16Unloo1I98Gz7i2B1bDjtBv9Du9vTE/ng9FkUPwcRhz9iGDDadUUgX\nBJGjpLqui2nNcg8HpPXPmDkgCrqO8i/JANmmaZIjAoGARCq1EKdyWYhBj5MsKzo8\nxrG+N5SuXx+SJGU+fxM1xsEnkfKMBAaDHqfpeRQcwoujDZ2mBu9yIsDlT2v1d2Dj\nWpXqLfIPXIbDKSt4wXpowfB/PM9tMOoc8ROsqiKa0VNgGm8hlr7IKU32etoSOmNb\nhuMax/ePIkgRHXFwbEhJzn0=\n-----END PRIVATE KEY-----\n",
+        "client_email": "firebase-adminsdk-tgp5t@study-start-d6061.iam.gserviceaccount.com",
+        "client_id": "100920437462379924865",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://accounts.google.com/o/oauth2/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-tgp5t%40study-start-d6061.iam.gserviceaccount.com"
+    })
+});
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
     const agent = new WebhookClient({
@@ -57,6 +71,9 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
 
     function testFunc(agent) {
         let topic = parameters.topic.toLowerCase();
+        if (topic === 'starter hacks') {
+            topic = 'starterhacks';
+        }
         let config = {
             headers: {
                 'Content-Type': 'text/plain'
